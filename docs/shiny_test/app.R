bd233ee4ace393ead3104be15f92a371c20ea82d
negatoscope <luiseudave@gmail.com>
2020-06-05 22:41:11 +0200

    0406-9

--- docs/shiny_test/app.R
+++ docs/shiny_test/app.R
@@ -1,50 +1,42 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(miniUI)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+ui <- miniPage(
+  gadgetTitleBar("Shiny gadget example"),
+  miniTabstripPanel(
+    miniTabPanel("Parameters", icon = icon("sliders"),
+                 miniContentPanel(
+                   sliderInput("year", "Year", 1978, 2010, c(2000, 2010), sep = "")
+                 )
+    ),
+    miniTabPanel("Visualize", icon = icon("area-chart"),
+                 miniContentPanel(
+                   plotOutput("cars", height = "100%")
+                 )
+    ),
+    miniTabPanel("Data", icon = icon("table"),
+                 miniContentPanel(
+                   DT::dataTableOutput("table")
+                 )
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+server <- function(input, output, session) {
+  output$cars <- renderPlot({
+    require(ggplot2)
+    ggplot(cars, aes(speed, dist)) + geom_point()
+  })
+  
+  output$table <- DT::renderDataTable({
+    diamonds
+  })
+  
+  observeEvent(input$done, {
+    stopApp(TRUE)
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
-
+#runGadget(shinyApp(ui, server), viewer = paneViewer())
+shinyApp(ui, server)
